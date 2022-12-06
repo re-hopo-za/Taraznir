aba87a961dceb732dbfa351e99bd9b3aaee13d4d
re-hopo-za <re-hopo-za@gmail.com>
2022-12-05 22:41:00 -0800

    dynamic background

--- app/Http/Livewire/Components/About/AboutSection.php
+++ app/Http/Livewire/Components/About/AboutSection.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Components\About;
 
+use App\Models\Option;
+use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 
 class AboutSection extends Component
 {
     public function render()
     {
-        return view('components.about.about-section');
+        $welcome = Cache::tags(['welcome'])->rememberForever( 'welcome' ,function (){
+            return Option::where('key' ,'welcome_background')->first();
+        });
+        return view('components.about.about-section',[
+            'welcome' => $welcome?->attachment()
+        ]);
     }
 }
