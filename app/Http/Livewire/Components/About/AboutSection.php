8984e5cb53312eb589c61e3dbe25830ae139326a
re-hopo-za <re-hopo-za@gmail.com>
2022-12-21 03:29:59 +0330

    change cache

--- app/Http/Livewire/Components/About/AboutSection.php
+++ app/Http/Livewire/Components/About/AboutSection.php
@@ -10,7 +10,10 @@ class AboutSection extends Component
 {
     public function render()
     {
-        $welcome = Cache::tags(['welcome'])->rememberForever( 'welcome' ,function (){
+//        $welcome = Cache::tags(['welcome'])->rememberForever( 'welcome' ,function (){
+//            return Option::where('key' ,'welcome_background')->first();
+//        });
+        $welcome = Cache::rememberForever( 'welcome' ,function (){
             return Option::where('key' ,'welcome_background')->first();
         });
         return view('components.about.about-section',[
