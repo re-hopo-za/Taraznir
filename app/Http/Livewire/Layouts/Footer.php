e048ea4bcf4e6f358311acac7a1ce120f32c8a70
re-hopo-za <re-hopo-za@gmail.com>
2022-12-21 03:34:22 +0330

    change cache

--- app/Http/Livewire/Layouts/Footer.php
+++ app/Http/Livewire/Layouts/Footer.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Layouts;
 
 use App\Models\Blog;
 use App\Models\Category;
+use App\Models\Option;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 
@@ -11,16 +12,23 @@ class Footer extends Component
 {
     public function render()
     {
-        $gallery = Cache::tags(['gallery'])->rememberForever( 'footer_gallery' ,function (){
+        $gallery = Cache::rememberForever( 'footer_gallery' ,function (){
             return Category::with(['blogs'])->where('slug' ,'footer_gallery' )
                 ->take(3)->get();
         });
-        $categories = Cache::tags(['cats'])->rememberForever( 'footer_service' ,function (){
+        $categories = Cache::rememberForever( 'footer_service' ,function (){
             return Category::where('model' ,'service')->take(8)->get();
         });
-        $recentPosts = Cache::tags(['blog'])->rememberForever( 'footer_recent_blog' ,function (){
+//        $recentPosts = Cache::tags(['blog'])->rememberForever( 'footer_recent_blog' ,function (){
+//            return  Blog::take(2)->orderBy('chosen' ,'asc')->get();
+//        });
+
+        $recentPosts = Cache::rememberForever( 'footer_recent_blog' ,function (){
             return  Blog::take(2)->orderBy('chosen' ,'asc')->get();
         });
+//        $sliders = redisHandler( 'footer' ,function (){
+//            return Option::where('key' ,'sliding_item')->get();
+//        });
 
         return view('layouts.footer' ,[
             'galleries'   => $gallery ,
