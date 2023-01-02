0673f66999553d5029e07783f75872fbe9716d8e
re-hopo-za <re-hopo-za@gmail.com>
2023-01-02 14:29:46 +0330

    commands

---
+++ app/Console/Commands/CacheAll.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class CacheAll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cache:all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make All Cache';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        echo Artisan::call('view:cache');
+        echo Artisan::call('config:cache');
+        echo Artisan::call('route:cache');
+        echo Artisan::call('event:cache');
+    }
+}
