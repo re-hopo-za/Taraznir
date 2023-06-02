9a3b694354352863709dfc7320bb60c93dac475f
Re-Hopo <re.hopo.za@gmail.com>
2023-06-02 21:01:45 +0330

    completed home sections

---
+++ src/components/features/AnotherSliderSection.tsx
@@ -0,0 +1,152 @@
+
+
+export default function AnotherSliderSection(){
+
+    return(
+        <section id="in-blog" className="in-blog-section">
+            <div className="container">
+                <div className="in-why-choose-top-content d-flex justify-content-between align-items-center">
+                    <div
+                        className="in-section-title-1 headline wow fadeInUp"
+                        data-wow-delay="0ms"
+                        data-wow-duration="1000ms"
+                        style={{
+                            visibility: "visible",
+                            animationDuration: "1000ms",
+                            animationDelay: "0ms",
+                            animationName: "fadeInUp"
+                        }}
+                    >
+                        <div className="sub-title position-relative text-uppercase">
+                            latest News
+                        </div>
+                        <h2>Our Latest News Post &amp; Articles </h2>
+                    </div>
+                    <div
+                        className="in-why-choose-top-text wow fadeInUp"
+                        data-wow-delay="150ms"
+                        data-wow-duration="1000ms"
+                        style={{
+                            visibility: "visible",
+                            animationDuration: "1000ms",
+                            animationDelay: "150ms",
+                            animationName: "fadeInUp"
+                        }}
+                    >
+                        On the other hand we denounce with righteous indignation and dislike men
+                        who are so beguiled and demoralized bite the finan charms of pleasure of
+                        blinded.
+                    </div>
+                </div>
+                <div className="in-blog-content">
+                    <div className="in-blog-slide">
+
+                        <div
+                            className="in-slider-item slick-slide slick-cloned"
+                            data-slick-index={-3}
+                            aria-hidden="true"
+                            tabIndex={-1}
+                            style={{ width: 401 }}
+                        >
+                            <div className="in-blog-item">
+                                <div className="blog-title headline">
+                                    <h3>
+                                        <a href="blog-single.html" tabIndex={-1}>
+                                            Winged Moved Star Creature Mark Seed Night.
+                                        </a>
+                                    </h3>
+                                </div>
+                                <div className="blog-img">
+                                    <img src="images/resource/news-5.jpg" alt="" />
+                                </div>
+                                <div className="blog-meta-text">
+                                    <div className="in-meta">
+                                        <a href="#" tabIndex={-1}>
+                                            <i className="far fa-calendar-check" /> 11 Jun 2022
+                                        </a>
+                                        <a href="#" tabIndex={-1}>
+                                            <i className="far fa-file-alt" /> Business
+                                        </a>
+                                    </div>
+                                    <div className="in-blog-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sedincididunt ut labore et dolore magna aliqua
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="in-slider-item slick-slide slick-cloned"
+                            data-slick-index={-2}
+                            aria-hidden="true"
+                            tabIndex={-1}
+                            style={{ width: 401 }}
+                        >
+                            <div className="in-blog-item">
+                                <div className="blog-title headline">
+                                    <h3>
+                                        <a href="blog-single.html" tabIndex={-1}>
+                                            Winged Moved Star Creature Mark Seed Night.
+                                        </a>
+                                    </h3>
+                                </div>
+                                <div className="blog-img">
+                                    <img src="images/resource/news-5.jpg" alt="" />
+                                </div>
+                                <div className="blog-meta-text">
+                                    <div className="in-meta">
+                                        <a href="#" tabIndex={-1}>
+                                            <i className="far fa-calendar-check" /> 11 Jun 2022
+                                        </a>
+                                        <a href="#" tabIndex={-1}>
+                                            <i className="far fa-file-alt" /> Business
+                                        </a>
+                                    </div>
+                                    <div className="in-blog-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sedincididunt ut labore et dolore magna aliqua
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="in-slider-item slick-slide slick-cloned"
+                            data-slick-index={-1}
+                            aria-hidden="true"
+                            tabIndex={-1}
+                            style={{ width: 401 }}
+                        >
+                            <div className="in-blog-item">
+                                <div className="blog-title headline">
+                                    <h3>
+                                        <a href="blog-single.html" tabIndex={-1}>
+                                            Winged Moved Star Creature Mark Seed Night.
+                                        </a>
+                                    </h3>
+                                </div>
+                                <div className="blog-img">
+                                    <img src="images/resource/news-5.jpg" alt="" />
+                                </div>
+                                <div className="blog-meta-text">
+                                    <div className="in-meta">
+                                        <a href="#" tabIndex={-1}>
+                                            <i className="far fa-calendar-check" /> 11 Jun 2022
+                                        </a>
+                                        <a href="#" tabIndex={-1}>
+                                            <i className="far fa-file-alt" /> Business
+                                        </a>
+                                    </div>
+                                    <div className="in-blog-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sedincididunt ut labore et dolore magna aliqua
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
